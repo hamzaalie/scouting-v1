8d0416b8467fc9ed5793b839ce3ff73c74a22958
hamzaalie <152795945+hamzaalie@users.noreply.github.com>
2026-01-12 17:03:36 +0500

    Remove missing lightbox CSS import

--- src/feature-module/pages/gallery.tsx
+++ src/feature-module/pages/gallery.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import {SlideshowLightbox} from "lightbox.js-react";
-import "lightbox.js-react/dist/index.css";
+// import "lightbox.js-react/dist/index.css"; // CSS not included in package
 import {all_routes} from "../router/all_routes";
 
 const Gallery = () => {
